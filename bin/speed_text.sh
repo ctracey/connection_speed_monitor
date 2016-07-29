96d9d22eb87ce07e4ee09e4ac5b4490d655863e2
tracer <tracer@192-168-1-11.tpgi.com.au>
2016-07-30 01:54:23 +1000

    graph connection speed from csv log

--- bin/speed_text.sh
+++ bin/speed_text.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-csv_file="log/speed_$(date '+%Y%m%d%H%M').csv"
+timestamp=$(date '+%Y%m')
+csv_file="log/speed_$timestamp.csv"
 mkdir -p 'log'
 
-./bin/speedtest_cli.py | grep -E "(Download|Upload)" | tr '\n' ' ' | awk -v date=$(date '+%Y%m%d%H%M') '{print date "," $2 "," $5}' >> $csv_file
-#cat ./test.log | grep -E "(Download|Upload)" | tr '\n' ' ' | awk -v date=$(date '+%Y%m%d%H%M') '{print date "," $2 "," $5}' >> $csv_file
+#./bin/speedtest_cli.py | grep -E "(Download|Upload)" | tr '\n' ' ' | awk -v date=$(date '+%Y%m%d%H%M') '{print date "," $2 "," $5}' >> $csv_file
+cat ./test.log | grep -E "(Download|Upload)" | tr '\n' ' ' | awk -v date=$(date '+%Y%m%d%H%M') '{print date "," $2 "," $5}' >> $csv_file
+
+cat $csv_file
 
 gnuplot << eor
 
  set terminal png size 900, 300
- set output 'out.png'
+ set output 'connection_speed_$timestamp.png'
+
+ set title "connection speed"
+ set ylabel "Mbps"
+ set xlabel "timestamp"
 
  set datafile separator ","
 
  set xdata time
- set timefmt "%Y%m%d"
+ set timefmt "%Y%m%d%H%M"
 
- plot "sample.csv" using 1:2 with line
+ plot "$csv_file" using 1:2 with line title "download", "$csv_file" using 1:3 with line title "upload"
 
 eor
 
