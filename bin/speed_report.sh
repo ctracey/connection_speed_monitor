87c3429edd21318bfbee8c6453192c82b89a222b
tracer <tracer@192-168-1-6.tpgi.com.au>
2016-07-30 23:36:05 +1000

    improving graph formatting

--- bin/speed_report.sh
+++ bin/speed_report.sh
@@ -16,7 +16,7 @@ gnuplot << eor
  set obj 1 fillstyle solid 1.0 fillcolor rgbcolor "black"
 
  set title "connection speed" textcolor rgbcolor "white"
- set key textcolor rgbcolor "white"
+ set key outside horizontal bmargin textcolor rgbcolor "white"
  set border linecolor rgbcolor "white"
  set ylabel "Mbps" textcolor rgbcolor "white"
  set xlabel "timestamp" textcolor rgbcolor "white"
@@ -30,7 +30,7 @@ gnuplot << eor
  set style line 1 lc rgb "blue"
  set style line 2 lc rgb "green"
 
- plot "$csv_file" using 1:2 with line title "download" ls 1, "$csv_file" using 1:3 with line title "upload" ls 2
+ plot "$csv_file" using 1:3 with line title "↑" ls 2, "$csv_file" using 1:2 with line title "↓" ls 1
 eor
 
 open $output_path/connection_speed_$timestamp.png
